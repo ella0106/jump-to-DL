05dbee5b543c7acad522f2057f357cb7774e975c
ella0106 <ella0106@naver.com>
2023-12-18 00:02:45 +0900

    update dezero.core

---
+++ dezero/functions.py
@@ -0,0 +1,38 @@
+import numpy as np
+from dezero.core import Function
+
+class Sin(Function):
+    def forward(self, x):
+        return np.sin(x)
+    
+    def backward(self, gy):
+        x, = self.inputs
+        gx = gy * cos(x)
+        return gx
+    
+def sin(x):
+    return Sin()(x)
+
+class Cos(Function):
+    def forward(self, x):
+        return np.cos(x)
+    
+    def backward(self, gy):
+        x, = self.inputs
+        gx = gy * -sin(x)
+        return gx
+    
+def cos(x):
+    return Cos()(x)
+
+class Tanh(Function):
+    def forward(self, x):
+        return np.tanh(x)
+    
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * (1 - y*y)
+        return gx
+    
+def tanh(x):
+    return Tanh()(x)
