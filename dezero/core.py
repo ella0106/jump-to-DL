a4d5c111f496aa28783f3bb18262227b11f97a85
ella <ella0106@naver.com>
2023-12-31 13:46:32 +0900

    Update core.py

--- dezero/core.py
+++ dezero/core.py
@@ -1,6 +1,5 @@
-import numpy as np
-import unittest
 import weakref
+import numpy as np
 import contextlib
 import dezero
 
@@ -104,6 +103,9 @@ class Variable:
     @property
     def T(self):
         return dezero.functions.transpose(self)
+    
+    def sum(x, axis=None, keepdims=False):
+        return dezero.functions.sum(self, axis, keepdims)
 
 def as_array(x):
     if np.isscalar(x):
@@ -144,11 +146,16 @@ class Function:
 
 class Add(Function):
     def forward(self, x0, x1):
+        self.x0_shape, self.x1_shape = x0.shape, x1.shape
         y = x0 + x1
         return y
     
     def backward(self, gy):
-        return gy, gy
+        gx0, gx1 = gy, gy
+        if self.x0_shape != self.x1_shape:
+            gx0 = dezero.functions.sum_to(gx0, self.x0_shape)
+            gx1 = dezero.functions.sum_to(gx1, self.x1_shape)
+        return gx0, gx1
 
 def add(x0, x1):
     x1 = as_array(x1)  
@@ -161,7 +168,12 @@ class Mul(Function):
     
     def backward(self, gy):
         x0, x1 = self.inputs
-        return gy * x1, gy * x0
+        gx0 = gy * x1
+        gx1 = gy * x0
+        if x0.shape != x1.shape:
+            gx0 = dezero.functions.sum_to(gx0, x0.shape)
+            gx1 = dezero.functions.sum_to(gx1, x1.shape)
+        return gx0, gx1
 
 def mul(x0, x1):
     x1 = as_array(x1) 
@@ -179,10 +191,15 @@ def neg(x):
 
 class Sub(Function):
     def forward(self, x0, x1):
+        self.x0_shape, self.x1_shape = x0.shape, x1.shape
         return x0 - x1
     
     def backward(self, gy):
-        return gy, -gy
+        gx0, gx1 = gy, -gy
+        if self.x0_shape != self.x1_shape:
+            gx0 = dezero.functions.sum_to(gx0, self.x0_shape)
+            gx1 = dezero.functions.sum_to(gx1, self.x1_shape)
+        return gx0, gx1
     
 def sub(x0, x1):
     x1 = as_array(x1) 
@@ -200,6 +217,9 @@ class Div(Function):
         x0, x1 = self.inputs
         gx0 = gy / x1
         gx1 = gy * (-x0 / x1 ** 2)
+        if x0.shape != x1.shape:
+            gx0 = dezero.functions.sum_to(gx0, x0.shape)
+            gx1 = dezero.functions.sum_to(gx1, x1.shape)
         return gx0, gx1
     
 def div(x0, x1):
