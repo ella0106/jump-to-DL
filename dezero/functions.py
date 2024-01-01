e8ba5a5ef8fc4b39b9459737dac8ce8c020d5204
ella <ella0106@naver.com>
2024-01-01 21:41:44 +0900

    Update functions.py

--- dezero/functions.py
+++ dezero/functions.py
@@ -1,5 +1,7 @@
 import numpy as np
-from dezero.core import Function, as_variable
+import dezero
+from dezero import utils
+from dezero.core import Function, Variable, as_variable, as_array
 
 class Sin(Function):
     def forward(self, x):
@@ -55,7 +57,7 @@ def reshape(x, shape):
     return Reshape(shape)(x)
 
 class Transpose(Function):
-    def __init(self, axes=None):
+    def __init__(self, axes=None):
         self.axes = axes
 
     def forward(self, x):
@@ -73,14 +75,55 @@ def transpose(x, axes=None):
     return Transpose(axes)(x)
 
 class Sum(Function):
+    def __init__(self, axis, keepdims) -> None:
+        self.axis = axis
+        self.keepdims = keepdims
+
     def forward(self, x):
         self.x_shape = x.shape
-        y = x.sum()
+        y = x.sum(axis=self.axis, keepdims=self.keepdims)
+        return y
+    
+    def backward(self, gy):
+        gy = utils.reshape_sum_backward(gy, self.x_shape, self.axis, self.keepdims)
+        gx = broadcast_to(gy, self.x_shape)
+        return gx
+    
+def sum(x, axis=None, keepdims=False):
+    return Sum(axis, keepdims)(x)
+
+class BroadcastTo(Function):
+    def __init__(self, shape):
+        self.shape = shape
+
+    def forward(self, x):
+        self.x_shape = x.shape
+        y = np.broadcast_to(x, self.shape)
+        return y
+    
+    def backward(self, gy):
+        gx = sum_to(gy, self.x_shape)
+        return gx
+    
+def broadcast_to(x, shape):
+    if x.shape == shape:
+        return as_variable(x)
+    return BroadcastTo(shape)(x)
+
+class SumTo(Function):
+    def __init__(self, shape):
+        self.shape = shape
+    
+    def forward(self, x):
+        self.x_shape = x.shape
+        y = utils.sum_to(x, self.shape)
         return y
     
     def backward(self, gy):
         gx = broadcast_to(gy, self.x_shape)
         return gx
     
-def sum(x):
-    return Sum()(x)
+def sum_to(x, shape):
+    if x.shape == shape:
+        return as_variable(x)
+    return SumTo(shape)(x)
