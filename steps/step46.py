b0139d38fe53087479de87f0470bd3e5aaa899d9
ella <ella0106@naver.com>
2024-04-20 00:48:14 +0900

    add step46

---
+++ steps/step46.py
@@ -0,0 +1,33 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from dezero import Model, optimizers
+from dezero.models import MLP
+import dezero.functions as F
+
+np.random.seed(0)
+x = np.random.rand(100, 1)
+y = np.sin(2 * np.pi * x) + np.random.rand(100, 1)
+
+lr = 0.2
+max_iter = 10000
+hidden_size = 10
+
+   
+model = MLP((hidden_size, 1))
+optimizer = optimizers.MomentumSGD(lr)
+optimizer.setup(model)
+
+# train
+for i in range(max_iter):
+    y_pred = model(x)
+    loss = F.mean_squared_error(y, y_pred)
+
+    model.cleargrads()
+    loss.backward()
+
+    optimizer.update()
+    if i % 1000 == 0:
+        print(loss)
